57369e1fe0bd9f0638422de58818c1e8292dacb3
wanyufu76 <wanyufu0706@gmail.com>
2025-08-11 22:53:27 +0800

    render

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,16 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
+    },
+  },
   root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
-  }
+  },
 });
